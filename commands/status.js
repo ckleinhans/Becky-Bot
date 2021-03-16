08e0c952504d533365f6f2a59754db6ca2c0fd7a
Caelan Kleinhans <55991831+ckleinhans@users.noreply.github.com>
2021-03-15 23:33:01 -0500

    Fix status command

--- commands/status.js
+++ commands/status.js
@@ -1,26 +1,27 @@
 const { client } = require("../index.js");
+
+const activityTypes = [
+  "playing",
+  "streaming",
+  "listening",
+  "watching",
+  "competing",
+  "custom",
+];
+
 module.exports = {
   name: "status",
   description: "Set my status activity or message!",
-  usage: `<${this.activityTypes.join("|")}> <name/message>`,
+  usage: `<${activityTypes.join("|")}> <name/message>`,
   cooldown: 5,
   args: true,
   serverOnly: false,
   adminOnly: true,
   aliases: ["setstatus"],
 
-  activityTypes: [
-    "playing",
-    "streaming",
-    "listening",
-    "watching",
-    "competing",
-    "custom",
-  ],
-
   execute(message, args) {
     activityType = args.shift().toLowerCase();
-    if (!this.activityTypes.includes(activityType)) {
+    if (!activityTypes.includes(activityType)) {
       return message.channel.send(
         `Improper usage of ${command.name}. Usage: ${config.prefix}${command.name} ${command.usage}`
       );
@@ -32,6 +33,8 @@ module.exports = {
         type: activityType.toUpperCase(),
       });
     }
-    console.log(`Set status to ${activityType.toUpperCase()}: ${args.join(' ')}`);
+    console.log(
+      `Set status to ${activityType.toUpperCase()}: ${args.join(" ")}`
+    );
   },
 };
